63f709906a343a2896cef5aa6eb2307e718745b7
Soloman Garcia <soloman.garcia@gmail.com>
2020-09-01 12:05:00 -0600

    add prettier

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('hello');
+app.get("/", (req, res) => {
+  res.send("hello");
 });
 
-app.listen();
+app.listen(8080);
