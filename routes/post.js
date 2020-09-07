6b07a3dba989174f63093018057f7713a6811fa7
Soloman Garcia <soloman.garcia@gmail.com>
2020-09-07 15:10:41 -0600

    refactor

--- routes/post.js
+++ routes/post.js
@@ -6,8 +6,8 @@ const { createPostValidator } = require("../validator/index");
 
 const router = express.Router();
 
-router.get("/", requireSignin, getPosts);
-router.post("/post", createPostValidator, createPost);
+router.get("/", getPosts);
+router.post("/post", requireSignin, createPostValidator, createPost);
 
 // any route containing :userId, app will first execute userById()
 router.param("userId", userById);
