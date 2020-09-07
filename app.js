bc45e45a97648e099cb79d3e96636661ac3d46bf
Soloman Garcia <soloman.garcia@gmail.com>
2020-09-07 12:05:02 -0600

    install cookieParser middleware

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express();
 const mongoose = require("mongoose");
 const morgan = require("morgan");
 const bodyParser = require("body-parser");
+var cookieParser = require("cookie-parser");
 const expressValidator = require("express-validator");
 const dotenv = require("dotenv");
 dotenv.config();
@@ -28,6 +29,7 @@ const authRoutes = require("./routes/auth.js");
 //middleware
 app.use(morgan("dev"));
 app.use(bodyParser.json());
+app.use(cookieParser());
 app.use(expressValidator());
 app.use("/", postRoutes);
 app.use("/", authRoutes);
