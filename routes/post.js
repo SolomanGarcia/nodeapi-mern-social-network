a3c964008330b2c05dc50180971112587251fff9
Soloman Garcia <soloman.garcia@gmail.com>
2020-09-07 14:58:25 -0600

    add userId to req param

--- routes/post.js
+++ routes/post.js
@@ -1,11 +1,15 @@
 const express = require("express");
 const { getPosts, createPost } = require("../controllers/post");
 const { requireSignin } = require("../controllers/auth");
+const { userById } = require("../controllers/user");
 const { createPostValidator } = require("../validator/index");
 
 const router = express.Router();
 
 router.get("/", requireSignin, getPosts);
 router.post("/post", createPostValidator, createPost);
+
+// any route containing :userId, app will first execute userById()
+router.param("userId", userById);
 
 module.exports = router;
