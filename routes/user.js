2004445a76f3b3bdaa34daae7a2582784c646d3d
Soloman Garcia <soloman.garcia@gmail.com>
2020-09-24 09:56:58 -0600

    add unfollow routes/controllers

--- routes/user.js
+++ routes/user.js
@@ -7,18 +7,16 @@ const {
   deleteUser,
   userPhoto,
   addFollowing,
-  addFollower
+  addFollower,
+  removeFollowing,
+  removeFollower
 } = require("../controllers/user");
 const { requireSignin } = require("../controllers/auth");
 
-const router = express.Router(
-  "/user/follow",
-  requireSignin,
-  addFollowing,
-  addFollower
-);
+const router = express.Router();
 
-router.put();
+router.put("/user/follow", requireSignin, addFollowing, addFollower);
+router.put("/user/unfollow", requireSignin, removeFollowing, removeFollower);
 
 router.get("/users", allUsers);
 router.get("/user/:userId", requireSignin, getUser);
