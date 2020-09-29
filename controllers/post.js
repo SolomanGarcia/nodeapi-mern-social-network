57c557b49a78413c196a1d906edb1c112504db04
Soloman Garcia <soloman.garcia@gmail.com>
2020-09-29 08:38:26 -0600

    refactor controllers/post

--- controllers/post.js
+++ controllers/post.js
@@ -6,8 +6,9 @@ const _ = require("lodash");
 exports.postById = (req, res, next, id) => {
   Post.findById(id)
     .populate("postedBy", "_id name")
-    .populate("comments", "text created")
     .populate("comments.postedBy", "_id name")
+    .populate("postedBy", "_id name role")
+    .select("_id title body created likes comments photo")
     .exec((err, post) => {
       if (err || !post) {
         return res.status(400).json({
@@ -19,20 +20,48 @@ exports.postById = (req, res, next, id) => {
     });
 };
 
-exports.getPosts = (req, res) => {
-  const post = Post.find()
-    .populate("postedBy", "_id name")
-    .populate("comments", "text created")
-    .populate("comments.postedBy", "_id name")
-    .select("_id title body created, likes")
-    .sort({ created: -1 })
+// exports.getPosts = (req, res) => {
+//   const post = Post.find()
+//     .populate("postedBy", "_id name")
+//     .populate("comments", "text created")
+//     .populate("comments.postedBy", "_id name")
+//     .select("_id title body created, likes")
+//     .sort({ created: -1 })
+//     .then((posts) => {
+//       res.json(posts);
+//     })
+//     .catch((err) => console.log(err));
+// };
+
+// with pagination
+exports.getPosts = async (req, res) => {
+  // get current page from req.query or use default value of 1
+  const currentPage = req.query.page || 1;
+  // return 3 posts per page
+  const perPage = 6;
+  let totalItems;
+
+  const posts = await Post.find()
+    // countDocuments() gives you total count of posts
+    .countDocuments()
+    .then((count) => {
+      totalItems = count;
+      return Post.find()
+        .skip((currentPage - 1) * perPage)
+        .populate("comments", "text created")
+        .populate("comments.postedBy", "_id name")
+        .populate("postedBy", "_id name")
+        .select("_id title body created likes")
+        .limit(perPage)
+        .sort({ created: -1 });
+    })
     .then((posts) => {
-      res.json(posts);
+      res.status(200).json(posts);
     })
     .catch((err) => console.log(err));
 };
 
-exports.createPost = (req, res) => {
+exports.createPost = (req, res, next) => {
   let form = new formidable.IncomingForm();
   form.keepExtensions = true;
   form.parse(req, (err, fields, files) => {
@@ -78,8 +107,15 @@ exports.postsByUser = (req, res) => {
 };
 
 exports.isPoster = (req, res, next) => {
-  let isPoster = req.post && req.auth && req.post.postedBy._id == req.auth._id;
-  if (!poster) {
+  let sameUser = req.post && req.auth && req.post.postedBy._id == req.auth._id;
+  let adminUser = req.post && req.auth && req.auth.role === "admin";
+
+  // console.log("req.post ", req.post, " req.auth ", req.auth);
+  // console.log("SAMEUSER: ", sameUser, " ADMINUSER: ", adminUser);
+
+  let isPoster = sameUser || adminUser;
+
+  if (!isPoster) {
     return res.status(403).json({
       error: "User is not authorized"
     });
@@ -235,4 +271,35 @@ exports.uncomment = (req, res) => {
         res.json(result);
       }
     });
+};
+
+exports.updateComment = (req, res) => {
+  let comment = req.body.comment;
+
+  Post.findByIdAndUpdate(req.body.postId, {
+    $pull: { comments: { _id: comment._id } }
+  }).exec((err, result) => {
+    if (err) {
+      return res.status(400).json({
+        error: err
+      });
+    } else {
+      Post.findByIdAndUpdate(
+        req.body.postId,
+        { $push: { comments: comment, updated: new Date() } },
+        { new: true }
+      )
+        .populate("comments.postedBy", "_id name")
+        .populate("postedBy", "_id name")
+        .exec((err, result) => {
+          if (err) {
+            return res.status(400).json({
+              error: err
+            });
+          } else {
+            res.json(result);
+          }
+        });
+    }
+  });
 };
