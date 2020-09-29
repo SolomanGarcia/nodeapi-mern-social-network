2357a9619e770c1a630075a77d58de2c33159fbe
Soloman Garcia <soloman.garcia@gmail.com>
2020-09-29 08:26:14 -0600

    refactor

--- models/user.js
+++ models/user.js
@@ -34,7 +34,11 @@ const userSchema = new mongoose.Schema({
     trim: true
   },
   following: [{ type: ObjectId, ref: "User" }],
-  followers: [{ type: ObjectId, ref: "User" }]
+  followers: [{ type: ObjectId, ref: "User" }],
+  resetPasswordLink: {
+    data: String,
+    default: ""
+  }
 });
 
 // virtual field
--- routes/auth.js
+++ routes/auth.js
@@ -1,13 +1,28 @@
 const express = require("express");
-const { signup, signin, signout } = require("../controllers/auth");
+const {
+  signup,
+  signin,
+  signout,
+  forgotPassword,
+  resetPassword
+} = require("../controllers/auth");
+
+const {
+  userSignupValidator,
+  userSigninValidator,
+  passwordResetValidator
+} = require("../validator/index");
 const { userById } = require("../controllers/user");
-const { userSignupValidator } = require("../validator/index");
 
 const router = express.Router();
 
 router.post("/signup", userSignupValidator, signup);
-router.post("/signin", signin);
+router.post("/signin", userSigninValidator, signin);
 router.get("/signout", signout);
+
+// password forgot and reset routes
+router.put("/forgot-password", forgotPassword);
+router.put("/reset-password", passwordResetValidator, resetPassword);
 
 // any route containing :userId, app will first execute userById()
 router.param("userId", userById);
